dede416dd4c02dbbdb8f1409b8f504a4d0354ca3
Roman Nikitchenko <roman.nikitchenko@philips.com>
2025-07-29 09:16:29 +0200

    Explicit moviepy version.

--- data-processors/transcribe-tree/setup-whisper-env-3.11.sh
+++ data-processors/transcribe-tree/setup-whisper-env-3.11.sh
@@ -4,7 +4,9 @@ pyenv local 3.11.11
 python3 -m venv whisper-env-3-11
 source whisper-env-3-11/bin/activate
 
-pip install -U pip torch torchvision torchaudio
+# At some point I need to free from the moviepy and move whisper-mps to use 2.x.
+pip install -U pip torch torchvision torchaudio moviepy==1.0.3
 
 #pip install -U openai-whisper
-pip install -U whisper-mps
+#pip install -U whisper-mps
+pip install git+https://github.com/romnikit/whisper-mps.git
