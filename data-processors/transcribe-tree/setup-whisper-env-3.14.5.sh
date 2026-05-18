337ff38ca7c680fda023d2c93e0ac79fe4be43b5
Roman Nikitchenko <roman@nikitchenko.dp.ua>
2026-05-18 09:03:20 +0200

    Python 3.14.5 is now the main version.

---
+++ data-processors/transcribe-tree/setup-whisper-env-3.14.5.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Needed Homebrew packages.
+# brew install xz
+
+# Whisper-mps actually require Python >= 3.10.
+PYTHON_VERSION="3.14.5"
+pyenv install -s $PYTHON_VERSION
+pyenv shell $PYTHON_VERSION
+python3 -m venv "whisper-env-$PYTHON_VERSION"
+source "whisper-env-$PYTHON_VERSION/bin/activate"
+
+# At some point I need to free from the moviepy and move whisper-mps to use 2.x.
+pip install -U pip torch torchvision torchaudio
+# moviepy==1.0.3
+
+#pip install -U openai-whisper
+pip install -U whisper-mps
+#pip install git+https://github.com/romnikit/whisper-mps.git
